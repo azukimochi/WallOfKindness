de12d2448eba6d0c1870b1c4d766b66f57d22400
aboozar mojdeh <aboozar.mojdeh@gmail.com>
2018-10-22 12:42:45 -0400

    first version of Database Model

---
+++ models/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	Walls: require("./Walls"),
+	guestMessage: require("./guestMessage"),
+	Category: require("./Category"),
+	SavedSearches: require("./SavedSearches"),
+	SavedAngels: require("./SavedAngels")
+};
