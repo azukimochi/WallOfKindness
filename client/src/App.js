410e018219140d1e09aacb474371a4511bd3306c
Apbh <bhattacharya_a@hotmail.com>
2018-10-21 23:01:23 -0400

    started adding components in src folder and added the pages folder with main and search files

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import About from "./pages/About";
+// import Search from "./pages/Search";
+import Main from "./pages/Main";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={Main} />
+        {/* <Route exact path="/about" component={About} /> */}
+        <Route exact path="/search" component={Search} />
+        {/* <Route exact path="/donerPage" component={DonerPage} /> */}
+        {/* <Route exact path="/signIn" component={signIn} /> */}
+        {/* <Route exact path="/signOut" component={signOut} /> */}
+      </Wrapper>
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default App;
+
