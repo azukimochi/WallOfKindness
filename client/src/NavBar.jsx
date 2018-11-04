95f047004fcd96669f83daa247be0c2caf542882
Apbh <bhattacharya_a@hotmail.com>
2018-11-04 11:24:14 -0500

    added css styling for main page

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -5,108 +5,122 @@ import "./Navbar.css";
 const NavBar = (props) => {
 	return (
 		<div>
-		<nav className="navbar navbar-inverse">
-		<div className="container-fluid">
-		<div className="navbar-header customNav">
-      <a className="navbar-brand " href="#">Wall of Kindness</a>
-    </div>
-		{/* <div className='NavBar'> */}
+			<nav className="navbar navbar-inverse">
+				<div className="container-fluid wrapperNav">
+					<div className="navbar-header customNav">
+						<a className="navbar-brand " href="#">Wall of Kindness</a>
+					</div>
+					{/* <div className='NavBar'> */}
 
-		<ul className="nav navbar-nav">
+					{/* <ul className="nav navbar-nav"> */}
 
-			<li className={
+						{/* <li className={
+    			window.location.pathname === "/" || window.location.pathname === "/home"
+     			 ? "active"
+     			 : ""
+  			}>
+    			<NavLink to="/" >Home</NavLink>
+    		</li> */}
+						{props.currentUser
+							? (
+								<div>
+									<ul className="nav navbar-nav">
+										<li className={
+											window.location.pathname === "/" || window.location.pathname === "/home"
+												? "active"
+												: ""
+										}>
+											<NavLink to="/" >Home</NavLink>
+										</li>
+										<li className={
+											window.location.pathname === "/search"
+												? "active"
+												: ""
+										}>
+											<NavLink to="/search" >Search</NavLink>
+										</li>
+
+										<li className={
+											window.location.pathname === "/dashboard"
+												? "active"
+												: ""
+										}>
+											<NavLink to="/dashboard" >Dashboard</NavLink>
+										</li>
+										{/* <NavLink to="/vip">VIP</NavLink> */}
+										{/* <NavLink to="/logout">Log Out</NavLink> */}
+
+										<li className={
+											window.location.pathname === "/messages"
+												? "active"
+												: ""
+										}>
+											<NavLink to="messages">Inbox</NavLink>
+										</li>
+									</ul>
+									<ul className="nav navbar-nav navbar-right">
+
+										<li>
+											<a href="/logout" className="nav-link" >
+												<img src=""
+													className="rounded-circle"
+													style={{ width: '25px', marginRight: '5px' }} />
+												<span className="glyphicon glyphicon-log-out"></span> Logout
+				   </a>
+										</li>
+									</ul>
+
+
+
+								</div>
+
+							)
+							: (
+								<div>
+									<ul className="nav navbar-nav">
+									<li className={
     			window.location.pathname === "/" || window.location.pathname === "/home"
      			 ? "active"
      			 : ""
   			}>
     			<NavLink to="/" >Home</NavLink>
     		</li>
-			{props.currentUser
-				? (
-					<div>
-					<ul className="nav navbar-nav">
-					<li className={
-    					window.location.pathname === "/search"
-      					? "active"
-     			 		: ""
-  					}>
-   			 		<NavLink to="/search" >Search</NavLink>
-    				</li>
-
-					 <li className={
-    					window.location.pathname === "/dashboard"
-    				  ? "active"
-    				  : ""
- 					 }>
-				    <NavLink to="/dashboard" >Dashboard</NavLink>
-    				</li>
-						{/* <NavLink to="/vip">VIP</NavLink> */}
-						{/* <NavLink to="/logout">Log Out</NavLink> */}
-
-						 <li className={
-    					window.location.pathname === "/messages"
-    					? "active"
-    					: ""
- 						 }>
-      				<NavLink to="messages">Inbox</NavLink>
-   					 </li>
-					</ul>
-					<ul className="nav navbar-nav navbar-right">
-       
-	   <li>
-	   <a href="/logout" className="nav-link" >
-					   <img src=""
-						   className="rounded-circle"
-						   style={{ width: '25px', marginRight: '5px'}} />
-							   <span className="glyphicon glyphicon-log-out"></span> Logout
-				   </a>
-				   </li>
-	   </ul>
 
 
+										<li className={
+											window.location.pathname === "/search"
+												? "active"
+												: ""
+										}>
+											<NavLink to="/search" >Search</NavLink>
+										</li>
+									</ul>
 
-					</div>
-					
-				)
-				: (
-					<div>
-					<ul className="nav navbar-nav">
+									<ul className="nav navbar-nav navbar-right">
+										<li className={
+											window.location.pathname === "/signup"
+												? "active"
+												: ""
+										}>
+											<NavLink to="/signup" ><span className="glyphicon glyphicon-user"></span> Sign Up</NavLink>
+										</li>
 
+										<li className={
+											window.location.pathname === "/login"
+												? "active"
+												: ""
+										}>
+											<NavLink to="/login" ><span className="glyphicon glyphicon-log-in"></span> Sign In</NavLink>
+										</li>
+									</ul>
+								</div>
 
-				<li className={
-				window.location.pathname === "/search"
-				? "active"
-				: ""
-				}>
-				<NavLink to="/search" >Search</NavLink>
-				</li>
-				</ul>
-				
-				<ul className="nav navbar-nav navbar-right">
-				<li className={
-				window.location.pathname === "/signup" 
-				? "active"
-				: ""
-				}>
-				<NavLink to="/signup" ><span className="glyphicon glyphicon-user"></span> Sign Up</NavLink>
-				</li>
+							)
+						}
 
-				<li className={
-				window.location.pathname === "/login" 
-				? "active"
-				: ""
-				}>
-				<NavLink to="/login" ><span className="glyphicon glyphicon-log-in"></span> Sign In</NavLink>
-				</li>
-				</ul>
-    			</div>
-				
-				)
-			}
-		
-		</ul>
-		</div>
-		</nav>
+					{/* </ul> */}
+				</div>
+			</nav>
 		</div>
 	)
 }
