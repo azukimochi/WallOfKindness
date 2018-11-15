3af950af34932f25b17da089e9d5452eaa5f9385
Durian <karen.kua7@gmail.com>
2018-11-14 22:23:21 -0500

    simplified the dashboard by taking away dashboard.js.  Just directed all props to wallbody component which I made into a stateless component.

--- client/src/pages/Dashboard/DashboardPage.js
+++ client/src/pages/Dashboard/DashboardPage.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import httpClient from '../../httpClient'
-import Dashboard from "./Dashboard.js";
+// import Dashboard from "./Dashboard.js";
+import WallBody from '../../components/WallBody/WallBody.js';
 import API from "../../utils/API.js"
 
 class DashboardPage extends Component {
   // state = {
   //    currentUser: httpClient.getCurrentUser()
   // };
-  
+
   state = {
     categories: [],
     city: "",
-    email: "karen@gmail.com",
+    email: "",
     gifts: [],
-    isDonor: false,
+    isDonor: "",
     name: "Karen",
     wallName: "",
-    zipCode: ""
+    zipCode: "",
+    id: ""
   }
 
 
@@ -25,60 +27,86 @@ class DashboardPage extends Component {
     console.log("id", id)
     API.getUserInfo(id)
       .then(res => {
-        console.log(res)
+        console.log(res.data)
+        this.setState({
+          categories: res.data.categories,
+          city: res.data.city,
+          email: res.data.email,
+          gifts: res.data.gifts,
+          isDonor: res.data.isDonor,
+          name: res.data.name,
+          wallName: res.data.wallName,
+          zipCode: res.data.zipCode,
+          id: res.data._id
+        }, ()=> console.log(this.state))
       })
       .catch(err => console.log(err))
   }
 
-  // componentWillMount() {
-    // console.log('gifts', localStorage.getItem("updatedGifts"));
-    // console.log('name', localStorage.getItem('updatedEmail'));
-
-  // }
-
-
-  updateWall = (e) => {
-    e.preventDefault();
-
-    //const { name, wallName, email, city, zipCode, categories, gifts, _id } = this.state.currentUser;
-
-    httpClient.updateUser(this.state.currentUser)
-      .then(user => {
-        this.setState({ currentUser: this.state.currentUser })
-        console.log('USER', this.state.currentUser);
-      })
-
-
-    console.log('current wall info', this.state.currentUser);
-    this.updateButtonEffect()
+  updateWall = event => {
+    event.preventDefault();
+    console.log("Hi, I'm the update button")
   }
-
+  
   updateButtonEffect() {
     let updateEffect = document.getElementById('updateWallButton');
     updateEffect.classList.add('running');
     setTimeout(function () { updateEffect.classList.remove('running') }, 2000);
   }
 
-  addClicked = e => {
-    e.preventDefault();
-    // console.log("e.target", e.target);
-    // console.log("kalle kiri", this.state);
-    let itemClicked = e.target.id;
-    let currentState = this.state.currentUser[itemClicked];
-    // console.log("current state", currentState);
-    currentState.push("");
-    this.setState({ itemClicked: currentState });
-  };
-
-  removeClicked(e) {
-    e.preventDefault();
-    let itemClicked = e.target.dataset.attribute;
-    console.log("e.target.dataset", e.target.dataset)
-    let itemGroup = e.target.dataset.group;
-    let currentState = this.state.currentUser[itemGroup];
-    currentState.splice(itemClicked, 1);
-    this.setState({ itemGroup: currentState });
+  addMoreItems = event => {
+    event.preventDefault();
+    console.log("Hi, I'm the addMoreItems button")
   }
+  
+  removeItem = event => {
+    event.preventDefault();
+    console.log("Hi, I'm the removeItem button")
+  }
+
+  // componentWillMount() {
+    // console.log('gifts', localStorage.getItem("updatedGifts"));
+    // console.log('name', localStorage.getItem('updatedEmail'));
+  // }
+
+
+  // updateWall = (e) => {
+  //   e.preventDefault();
+
+    //const { name, wallName, email, city, zipCode, categories, gifts, _id } = this.state.currentUser;
+
+  //   httpClient.updateUser(this.state.currentUser)
+  //     .then(user => {
+  //       this.setState({ currentUser: this.state.currentUser })
+  //       console.log('USER', this.state.currentUser);
+  //     })
+
+
+  //   console.log('current wall info', this.state.currentUser);
+  //   this.updateButtonEffect()
+  // }
+
+
+  // addClicked = e => {
+  //   e.preventDefault();
+  //   // console.log("e.target", e.target);
+  //   // console.log("kalle kiri", this.state);
+  //   let itemClicked = e.target.id;
+  //   let currentState = this.state.currentUser[itemClicked];
+  //   // console.log("current state", currentState);
+  //   currentState.push("");
+  //   this.setState({ itemClicked: currentState });
+  // };
+
+  // removeClicked(e) {
+  //   e.preventDefault();
+  //   let itemClicked = e.target.dataset.attribute;
+  //   console.log("e.target.dataset", e.target.dataset)
+  //   let itemGroup = e.target.dataset.group;
+  //   let currentState = this.state.currentUser[itemGroup];
+  //   currentState.splice(itemClicked, 1);
+  //   this.setState({ itemGroup: currentState });
+  // }
 
   itemChangeGifts = (e) => {
     let itemToChange = e.target.dataset.attribute;
@@ -93,22 +121,22 @@ class DashboardPage extends Component {
     console.log('input change', localStorage.getItem("updatedGifts"));
   };
 
-  itemChange = (e) => {
-    let itemToChange = e.target.dataset.attribute;
-    console.log("e.target", e.target)
-    const itemState = this.state.currentUser;
-    console.log("this.state.currentUser", this.state.currentUser)
-    const itemGroup = e.target.dataset.group;
-    const currentState = this.state[itemGroup];
-    itemState[e.target.name] = e.target.value;
-    this.setState({ currentUser: itemState });
+  // itemChange = (e) => {
+  //   let itemToChange = e.target.dataset.attribute;
+  //   console.log("e.target", e.target)
+  //   const itemState = this.state.currentUser;
+  //   console.log("this.state.currentUser", this.state.currentUser)
+  //   const itemGroup = e.target.dataset.group;
+  //   const currentState = this.state[itemGroup];
+  //   itemState[e.target.name] = e.target.value;
+  //   this.setState({ currentUser: itemState });
 
-    var storageName = localStorage.setItem("updatedName", itemState.name);
-    var storageWallName = localStorage.setItem("updatedWallName", itemState.wallName);
-    var storageEmail = localStorage.setItem("updatedEmail", itemState.email);
-    var storageCity = localStorage.setItem("updatedCity", itemState.city);
-    var storageZipCode = localStorage.setItem("updatedZipcode", itemState.zipCode);
-  }
+  //   var storageName = localStorage.setItem("updatedName", itemState.name);
+  //   var storageWallName = localStorage.setItem("updatedWallName", itemState.wallName);
+  //   var storageEmail = localStorage.setItem("updatedEmail", itemState.email);
+  //   var storageCity = localStorage.setItem("updatedCity", itemState.city);
+  //   var storageZipCode = localStorage.setItem("updatedZipcode", itemState.zipCode);
+  // }
 
 
 
@@ -117,29 +145,29 @@ class DashboardPage extends Component {
     return (
       <div>
         Hi
-        <Dashboard
+        <WallBody
          email={this.state.email}
           // user={this.state.currentUser.user}
-          name={this.state.name}
+          donor={this.state.name}
           categories={this.state.categories}
           gifts={this.state.gifts}
           wallName={this.state.wallName }
           zipCode={this.state.zipCode }
           city={this.state.city }
-          btnClickHandler={e => {
-            this.updateWall(e);
+          updateWall={event => {
+            this.updateWall(event);
           }}
-          addClick={e => {
-            this.addClicked(e);
+          addClick={event => {
+            this.addMoreItems(event);
           }}
-          removeClick={e => {
-            this.removeClicked(e);
+          removeClick={event => {
+            this.removeItem(event);
           }}
-          itemChanged={e => {
-            this.itemChange(e);
+          itemChanged={event => {
+            this.handleItemChange(event);
           }}
-          itemChangedGifts={e => {
-            this.itemChangeGifts(e);
+          itemChangedGifts={event => {
+            this.itemChangeGifts(event);
           }}
         />
 
