be851e69da208f2da757717316db4dc132c1fa36
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2018-12-03 17:02:03 -0500

    File modification-Part1

--- controllers/nodeMailer.js
+++ controllers/nodeMailer.js
@@ -1,28 +1,27 @@
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
-  var transporter = nodemailer.createTransport({
-    service: 'Yahoo',
-    auth: {
-      user: 'thewallofkindness',
-      pass: 'finalproject'
-    }
-  });
+var transporter = nodemailer.createTransport({
+  service: "Yahoo",
+  auth: {
+    user: "thewallofkindness",
+    pass: "finalproject"
+  }
+});
 
-  module.exports = {
-    sendMail: function(emailTo, emailFrom, emailSubject, emailBody){
-      var mailOptions = {
-        from: 'thewallofkindness@yahoo.com',
-        to: emailTo,
-        subject: emailSubject,
-        text: `${emailBody} Please reach back out to me at ${emailFrom}`
-      };
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-  
-    }
-  };
+module.exports = {
+  sendMail: function(emailTo, emailFrom, emailSubject, emailBody) {
+    var mailOptions = {
+      from: "thewallofkindness@yahoo.com",
+      to: emailTo,
+      subject: emailSubject,
+      text: `${emailBody} Please reach back out to me at ${emailFrom}`
+    };
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  }
+};
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,7 @@
 const router = require("express").Router();
-const WallRoutes = require("./walls");
-// const UserRoutes = require("./users");
-const send = require("./send"); 
 
-router.use("/walls", WallRoutes);
-// router.use("/users", UserRoutes);
-router.use("/send", send); 
+const send = require("./send");
+
+router.use("/send", send);
 
 module.exports = router;
