7d8060fece72cc34d19d661c547a463f995f2386
Durian <karen.kua7@gmail.com>
2018-12-12 22:04:45 -0500

    Cleaned up Search code

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -8,20 +8,13 @@ const SearchResults = props =>
         <div className="card text-center">
           <div className="card-header" id="resultsHeader" />
           <div className="card-body" id="resultsDiv">
-            {console.log("props is:", props)}
-
-            {/* <p className="card-text">Full Name: {props.name}</p> */}
-            <p className="card-text">Wall Name: {props.wallName.toUpperCase()}</p>
+            <p className="card-text">Wall Name: {props.wallName}</p>
             <p className="card-text"> Gift Name: {props.item}</p>
             <p className="card-text"> Date Posted (YYYY-MM-DD):</p>
             <p className="card-text">{props.date}</p>
             <p className="card-text"> Distance:</p>
             <p className="card-text">{props.distanceM} m</p>
             <p className="card-text">{props.distanceKM} km</p>
-            {/* <p className="card-text"> Email Address: {props.email}</p> */}
-            {/* <p className="card-text"> City: {props.city.toUpperCase()}</p> */}
-            {/* <p className="card-text"> Address: {props.address}</p> */}
-
             <button
               className="registerBtn "
               name={props.email}
@@ -29,10 +22,6 @@ const SearchResults = props =>
             >
               Request
             </button>
-
-            {/* <h4 className="invisible" id="toast">
-              Your email is being sent.
-            </h4> */}
           </div>
         </div>
       </div>
